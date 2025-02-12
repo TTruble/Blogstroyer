8406ea9264d248c82ade8fc762082291834848b3
TTruble <blazingaustin@gmail.com>
2025-02-12 16:45:36 +0200

    stuff for blogstroyer

--- backend/mailer.php
+++ backend/mailer.php
@@ -4,11 +4,11 @@ use SendinBlue\Client\Api\TransactionalEmailsApi;
 use SendinBlue\Client\Configuration;
 use SendinBlue\Client\Model\SendSmtpEmail;
 
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 
 function sendVerificationEmail($email, $code) {
     $config = Configuration::getDefaultConfiguration()
-        ->setApiKey('api-key', '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');
+        ->setApiKey('api-key', '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');
     
     $apiInstance = new TransactionalEmailsApi(
         new GuzzleHttp\Client(),
@@ -17,7 +17,7 @@ function sendVerificationEmail($email, $code) {
     
     $sendSmtpEmail = new SendSmtpEmail();
     $sendSmtpEmail['to'] = [['email' => $email]];
-    $sendSmtpEmail['sender'] = ['name' => 'Your Website Name', 'email' => 'blazingaustin@gmail.com'];
+    $sendSmtpEmail['sender'] = ['name' => 'BLOGSTROYER', 'email' => 'blazingaustin@gmail.com'];
     $sendSmtpEmail['subject'] = 'Email Verification Code';
     $sendSmtpEmail['htmlContent'] = "Your verification code is: <b>$code</b>";
     
@@ -25,13 +25,15 @@ function sendVerificationEmail($email, $code) {
         $apiInstance->sendTransacEmail($sendSmtpEmail);
         return true;
     } catch (Exception $e) {
+        print_r($e);
+
         return false;
     }
 }
 
 function sendPasswordResetEmail($email, $code) {
     $config = Configuration::getDefaultConfiguration()
-        ->setApiKey('api-key', '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');
+        ->setApiKey('api-key', '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');
     
     $apiInstance = new TransactionalEmailsApi(
         new GuzzleHttp\Client(),
@@ -40,7 +42,7 @@ function sendPasswordResetEmail($email, $code) {
     
     $sendSmtpEmail = new SendSmtpEmail();
     $sendSmtpEmail['to'] = [['email' => $email]];
-    $sendSmtpEmail['sender'] = ['name' => 'Your Website Name', 'email' => 'blazingaustin@gmail.com'];
+    $sendSmtpEmail['sender'] = ['name' => 'BLOGSTROYER', 'email' => 'blazingaustin@gmail.com'];
     $sendSmtpEmail['subject'] = 'Password Reset Code';
     $sendSmtpEmail['htmlContent'] = "Your password reset code is: <b>$code</b>";
     
